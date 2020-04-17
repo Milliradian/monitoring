45ac6e5e55a9bdcafcca2a09062e790003021cc8
mooradio <62661907+mooradio@users.noreply.github.com>
2020-04-17 19:56:11 +0400

    Create R_code_rs.r

---
+++ R_code_rs.r
@@ -0,0 +1,16 @@
+# R code for remote sensing data analysis
+
+# check/set working directory
+
+getwd()
+setwd("C:/lab/")
+
+# install the package raster
+install.packages("raster")
+install.packages("RStoolbox")
+
+# require packages
+library(raster)
+library(RStoolbox)
+
+
