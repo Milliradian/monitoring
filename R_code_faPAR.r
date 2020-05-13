a615ff701fb1f1b73a1218d4ab967f4598c146f5
mooradio <62661907+mooradio@users.noreply.github.com>
2020-05-13 19:52:13 +0400

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -36,6 +36,45 @@ pdf("faPAR10.pdf")
 levelplot(faPAR10)
 dev.off()
 
+#############################
+
+#2nd day
+
+# Set working directory
+# setwd("~/lab/")
+# setwd("/Users/utente/lab") #mac
+setwd("C:/lab/") # windows
+
+# load the workspace
+
+load(".RData")
+
+ls()
+
+faPAR10
+
+#require libraries
+
+library(raster)
+
+library(rasterdiv)
+
+library(rasterVis)
+
+# the original file of fapar from Copernicus is 2 GB
+
+# let's see how much space is needed for 8-BIT set
+
+
+# see the range of file copNDVI
+
+copNDVI 
+
+# write a raster file
+
+writeRaster(copNDVI, "copNDVI.tif") #5.3 MB, that's why we are using this files instead of original radiance files
+
+levelplot(faPAR10)
 
 
 
