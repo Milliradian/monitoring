84e1347fb9859bbe20cab8f3e130bff4cc82e8d3
mooradio <62661907+mooradio@users.noreply.github.com>
2020-05-27 19:29:17 +0400

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -1,4 +1,5 @@
 ########
+#1st Day
 
 # Download the data from the iol website (image is aggregated, 10 times)
 
@@ -75,3 +76,81 @@ plotRGB(snt,4,3,2, stretch="lin", main="the original image")
 plot(sd_snt, col=cl, main="diversity")
 
 # more diversity is observed on the borders of the different ecosystems
+
+########
+#2nd Day
+
+#Download the cladonia_stellaris_calaita.JPG from the IOL website to the lab folder
+
+# Set working directory
+# setwd("~/lab/")
+# setwd("/Users/utente/lab") #mac
+setwd("C:/lab/") # windows
+
+# require libraries
+library(raster)
+library(RStoolbox)
+
+# brick the cladonia_stellaris_calaita.JPG and assign a name 
+clad <- brick("cladonia_stellaris_calaita.JPG")
+
+#plot the image by using plotRGB function 
+
+plotRGB(clad, 1, 2, 3, stretch="lin") 
+
+
+
+# use pairs to the relation
+pairs(clad)
+
+# PCA Analysis for the cladonia
+
+cladpca <- rasterPCA(clad)
+
+# see the information of the pca analysis
+
+cladpca
+
+# see the summary of the pca analysis
+summary(cladpca$model) #98% by 1st component
+
+# plot the map of cladpca
+plotRGB(cladpca$map, 1, 2, 3, stretch="lin")
+
+
+
+#put the focal function on top of the image to measure biodiversity
+
+#build the moving window (3x3 matrix and set value for the pixel, in this case it is 1)
+
+window <- matrix(1, nrow = 3, ncol = 3)
+
+# check the window
+window
+
+# standard deviation of the clad
+
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
+
+# you can also aggregate the cladpca and then do the calculation of the standard deviation to accelerate the process
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+
+# create a colorramppalette 
+cl <- colorRampPalette(c("yellow", "violet", "black"))(100)
+
+# plot the calculations
+par(mfrow=c(1,2))
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl) # all of the microvariations in the structure of the cladonia can be seen in these images. 
+
+
+# For comparison
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad, 1,2,3, stretch="lin")
+plot(sd_clad, col=cl) #or/ plot(sd_clad_agg, col=cl)
+
+
+
+
