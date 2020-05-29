c11f6e979019b5b9d6611108db90b13264f4c15c
mooradio <62661907+mooradio@users.noreply.github.com>
2020-05-29 17:33:27 +0400

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -1,51 +1,77 @@
-# Point Pattern Analysis: Density map
+### Point Pattern Analysis: Density map
 
+# set the working directory: lab
+
+# Windows
+# setwd("C:/lab/")
+# Mac users
+# setwd("/Users/yourname/lab/")
+# Linux users
+setwd("~/lab")
+
+# install packages
 install.packages("spatstat")
-library(spatstat) 
+install.packages("rgdal")
 
-# attaching the dataset
+# require packages
+library(spatstat)
+library(rgdal)
+
+# import the data set (head=T means that there is a column header)
+covid <-read.table("covid_agg.csv",head=T)
+
+# attach the data set
 attach(covid)
 
 # check the first 6 lines (head) of the data
 head(covid)
 
-# 
-covids <- ppp(lon, lat, c(-180,180), c(-90,90))
+# set the coordinates of the vectors in covids in relation to the global map
+covids <-ppp(lon, lat,c(-180,180),c(-90,90)) # c for clustering
 
 # without attaching the covid set
-# covids <- ppp(covid$lon, covid$lat, c(-180,180), c(-90,90))
+## covids <- ppp(covid$lon, covid$lat, c(-180,180), c(-90,90))
 
+# density plots are usually a much more effective way to view the distribution of a variable. 
+## Create the plot using plot(density())
 d <- density(covids)
 
 plot(d)
+
+# draw a sequence of points 
 points(covids)
 
-setwd("C:/lab")
+#### Part 2
+# set the working directory: lab
 
-load(".RData")
+# Windows
+# setwd("C:/lab/")
+# Mac users
+# setwd("/Users/yourname/lab/")
+# Linux users
+setwd("~/lab")
 
+# load previous workspace
+load("point_pattern.RData")
+
+# display all the objects
 ls()
 
 # covids: point pattern
 # d: density map
 
-library(spatstat)
+library(spatstat) 
+library(rgdal) # in case you do no have the package, first install and then require it
 
-# in case you do no have the package, first instal it with install.packages("spatstat") and then require it
-
-# plot a density map
-
+# plot the density map
 plot(d)
 
 # putting points on the plot
 points(covids)
 
+# let’s input vector lines (x0y0, x1y1, x2y2..)
 # inputing the coastlines (download from the iol website and then extract to your working directory)
-
-install.packages("rgdal") 
-library("rgdal")
-
-# giving the name to the object and asigning the function and inputing the vector lines (x0y0, x1y1, x2y2..)
+# give the name to the object 
 coastlines <- readOGR("ne_10m_coastline.shp")
 
 # add coastlines to the plot
@@ -65,7 +91,7 @@ plot(d, col=cl, main="Densities of Covid-19")
 points(covids)
 plot(coastlines, add=T)
 
-# export pdf
+# export pdf file
 pdf("covid_density.pdf")
 clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
 plot(d, col=clr, main="Densities of covid-19")
@@ -73,7 +99,7 @@ points(covids)
 plot(coastlines, add=T)
 dev.off()
 
-# export png
+# export png file
 png("covid_density.png")
 clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
 plot(d, col=clr, main="Densities of covid-19")
