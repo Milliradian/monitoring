59d1175b838d11d874d6333126f97d3dcd084486
mooradio <62661907+mooradio@users.noreply.github.com>
2020-05-29 15:09:50 +0400

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -1,18 +1,30 @@
+# Introduction to the R Software and the Free and Open Source philosophy: how to deal with R making your first code!
+
+# install package 'sp'
 install.packages("sp")
 
+# require installed package
 library("sp")
+
+# call pre-loaded data set
 data(meuse)
 
-# Let's see how the meuse dataset is structured:
+# See how the meuse dataset is structured
 meuse
 
-# Let's look at the first rows of the set
+# Look at the first 6 rows of the set
 head(meuse)
 
-# Let's plot two variables
-# Let's see if the zinc concentration is related to that of copper
+# Plot two variables
+# See if the zinc concentration is related to that of copper
 attach(meuse)
 plot(zinc,copper)
+
+# set color to your plot by 'col' function
 plot(zinc,copper,col="green")
+
+# change symbols with 'pch' function
 plot(zinc,copper,col="green",pch=19)
+
+# change the scale of symbols with 'cex' function
 plot(zinc,copper,col="green",pch=19,cex=2)
