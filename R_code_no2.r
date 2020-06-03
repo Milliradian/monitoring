7cab0f6b3a8e65562c067679d71b1c9bb01b4f2c
mooradio <62661907+mooradio@users.noreply.github.com>
2020-06-03 20:03:09 +0400

    Create R_code_no2.r

---
+++ R_code_no2.r
@@ -0,0 +1,12 @@
+# NO2 levels change before and after the COVID-19
+# Download NO2 data (EN.zip) from the IOL website
+## create a folder called /no2 folder in the /lab folder and put all the extracted data into the /no2 folder
+
+# setting the working directory: no2
+
+# Windows users
+# setwd("C:/lab/no2")
+# Mac users
+# setwd("/Users/yourname/lab/no2")
+# Linux users
+setwd("~/lab/no2")
